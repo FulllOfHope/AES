de0797a3ebcf5dbae6bd95130114638fec48ad29
Niraj Hariyani <nirajhariyani3214@gmail.com>
2026-08-09 02:08:27 +0530

    Clean up comments in mix_columns.v
    Removed header comments and unnecessary whitespace.

--- mix_columns.v
+++ mix_columns.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2026 10:35:35
-// Design Name: 
-// Module Name: mix_columns
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mix_columns(
 input [127:0] data_in,
