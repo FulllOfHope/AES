bab563e2390cf26122f7fc00d6fd52eddc24167f
Niraj Hariyani <nirajhariyani3214@gmail.com>
2026-08-09 02:08:56 +0530

    Clean up header comments in top.v
    Removed header comments from top.v file.

--- top.v
+++ top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2026 20:43:25
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top(
     input clk,
